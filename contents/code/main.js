3d9c796aceed336d7ce7c5f6925cbbc18c6cd2a2
morj <morj@morj.men>
2024-03-08 19:32:22 +0100

    WIP compat bullshit

--- contents/code/main.js
+++ contents/code/main.js
@@ -5,15 +5,93 @@
 
 const MIN_DESKTOPS = 2;
 
+const isKde6 = typeof workspace.windowList === "function";
+const compat = isKde6
+	?
+		{ addDesktop = () =>
+			{ workspace.createDesktop(workspace.desktops.length, "dyndesk"); }
+		, windowAddedSignal = ws => ws.windowAdded
+		, windowList = ws => ws.windowList()
+		, desktopChangedSignal = c => c.desktopsChanged
+
+		, toDesktop = d => d
+		, workspaceDesktops = () => workspace.desktops
+		, lastDesktop = () => workspace.desktops[workspace.desktops.length - 1]
+		, deleteLastDesktop = () =>
+			{
+				const last = workspace.desktops[workspace.desktops.length - 1];
+				workspace.removeDesktop(last);
+			}
+		, findDesktop = (ds, d) => ds.indexOf(d)
+
+		, clientDesktops = c => c.desktops
+		, setClientDesktops = (c, ds) =>
+			{
+				c.desktops = ds;
+			}
+		, clientOnDesktop = (c, d) => client.desktops.indexOf(d) !== -1
+		}
+	:
+		{ addDesktop = () =>
+			{ workspace.desktops += 1; }
+		, windowAddedSignal = ws => ws.clientAdded
+		, windowList = ws => ws.clientList()
+		, desktopChangedSignal = c => c.desktopChanged
+
+		// emulate plasma 6 behaviour with custom types
+		, toDesktop = number =>
+			{
+				return
+					{ index: number - 1;
+					}
+			}
+		, workspaceDesktops = () =>
+			{
+				let r = [];
+				for (let i = 0; i < workspace.desktops; ++i)
+				{
+					const desktop =
+						{ index: i
+						};
+					r.push(desktop);
+				}
+				return r;
+			}
+		, lastDesktop = () =>
+			{
+				return { index: workspace.desktops - 1
+				       };
+			}
+		, deleteLastDesktop = () =>
+			{ workspace.removeDesktop(workspace.desktops - 1); }
+		, findDesktop = (ds, d) =>
+			{
+				for (let i = 0; i < ds.length; ++i)
+				{
+					if (ds[i].index == d.index)
+					{
+						return i;
+					}
+				}
+				return -1;
+			}
+
+		, clientDesktops = c =>
+			{
+				return [ { index: c.desktop - 1
+				         }
+				       ]
+			}
+		, setClientDesktops = (c, ds) =>
+			{
+				c.desktop = ds[0];
+			}
+		, clientOnDesktop = (c, d) => c.desktop === d.index + 1
+		};
+
 function log(...args)
 {
 	print("[dynamic_workspaces] ", ...args);
-}
-
-function add_desktop()
-{
-	log("add_desktop()");
-	workspace.createDesktop(workspace.desktops.length, "dyndesk");
 }
 
 // shifts a window to the left if it's more to the right than number
@@ -21,14 +99,14 @@ function shift_righter_than(client, number)
 {
 	// Build a new array by comparing old client desktops with all available
 	// desktops
-	const all_desktops = workspace.desktops;
-	const client_desktops = client.desktops;
+	const all_desktops = compat.workspaceDesktops();
+	const client_desktops = compat.clientDesktops(client);
 	let new_desktops = [];
 	// first add unchanged desktops
 	for (let i = 0; i <= number; ++i)
 	{
 		const d = all_desktops[i];
-		if (client_desktops.indexOf(d) != -1)
+		if (compat.findDesktop(client_desktops, d) != -1)
 		{
 			new_desktops.push(d);
 		}
@@ -37,13 +115,13 @@ function shift_righter_than(client, number)
 	for (let i = number + 1; i < all_desktops.length; ++i)
 	{
 		const d = all_desktops[i];
-		if (client_desktops.indexOf(d) != -1)
+		if (compat.findDesktop(client_desktops, d) != -1)
 		{
 			new_desktops.push(all_desktops[i-1]);
 		}
 	}
 
-	client.desktops = new_desktops;
+	compat.setClientDesktops(client, new_desktops);
 }
 
 /**
@@ -55,34 +133,33 @@ function remove_desktop_with(number)
 {
 	log(`remove_desktop_with(${number})`);
 
+	const desktopsLength = compat.workspaceDesktops().length;
 	// don't do anything if below minimum desktops
-	if (workspace.desktops.length <= MIN_DESKTOPS) return false;
-
+	if (desktopsLength <= MIN_DESKTOPS) return false;
 	// do not remove empty desktop at the end
-	if (workspace.desktops.length - 1 == number) return false;
+	if (desktopsLength - 1 == number) return false;
 
 	// plasma6 allows us to delete desktops in the middle. Unfortunately, this
 	// messes up pager, so we have to do what we did un plasma5 and shift all
 	// windows by hand to delete the last desktop
-	workspace.windowList().forEach((client) =>
+	compat.windowList(workspace).forEach((client) =>
 	{
 		shift_righter_than(client, number)
 	});
-	// remove last
-	workspace.removeDesktop(workspace.desktops[workspace.desktops.length - 1]);
+	compat.deleteLastDesktop();
 	return true;
 }
 
 // tells if desktop has no windows of its own
 function is_empty_desktop(number)
 {
-	const desktop = workspace.desktops[number];
+	const desktop = compat.workspaceDesktops()[number];
 	log(`is_empty_desktop(${number})`)
-	const cls = workspace.windowList();
+	const cls = compat.windowList(workspace);
 	cls.forEach(client =>
 	{
 		// is client on desktop?
-		if (client.desktops.indexOf(desktop) !== -1
+		if (compat.clientOnDesktop(client, desktop) !== -1
 			&& !client.skipPager // ignore hidden windows
 			&& !client.onAllDesktops // ignore windows on all desktops
 		) {
@@ -100,12 +177,12 @@ function is_empty_desktop(number)
  */
 function desktop_changed_for(client)
 {
-	log(`desktop_changed_for() -> Client ${client.caption} just moved to desktop number ${client.desktops}`);
+	log(`desktop_changed_for() -> Client ${client.caption} just moved`);
 
-	const last_desktop = workspace.desktops[workspace.desktops.length - 1];
-	if (client.desktops.indexOf(last_desktop) != -1)
+	const last_desktop = compat.lastDesktop();
+	if (compat.clientOnDesktop(client, last_desktop) != -1)
 	{
-		add_desktop();
+		compat.addDesktop();
 	}
 }
 
@@ -127,14 +204,14 @@ function on_client_added(client)
 	}
 
 	// add a new desktop for a client too right
-	const last_desktop = workspace.desktops[workspace.desktops.length - 1];
-	if (client.desktops.indexOf(last_desktop) != -1)
+	const last_desktop = compat.lastDesktop();
+	if (compat.clientOnDesktop(client, last_desktop) != -1)
 	{
-		add_desktop();
+		compat.addDesktop();
 	}
 
 	// subscribe the client to create desktops when desktop switched
-	client.desktopsChanged.connect(() => { desktop_changed_for(client); });
+	compat.desktopChangedSignal(client).connect(() => { desktop_changed_for(client); });
 }
 
 /**
@@ -144,8 +221,9 @@ function on_desktop_switch(old_desktop)
 {
 	log(`on_desktop_switch(${old_desktop})`);
 
-	const old_desktop_index = workspace.desktops.indexOf(old_desktop);
-	const current_desktop_index = workspace.desktops.indexOf(workspace.currentDesktop)
+	const allDesktops = compat.workspaceDesktops();
+	const old_desktop_index = compat.findDesktop(allDesktops, compat.toDesktop(old_desktop));
+	const current_desktop_index = compat.findDesktop(allDesktops, compat.toDesktop(workspace.currentDesktop));
 
 	// do nothing if we switched to the right
 	if (old_desktop_index <= current_desktop_index) return;
@@ -156,8 +234,8 @@ function on_desktop_switch(old_desktop)
 	// prevent infinite loop in case of an error - only try as many times as there are desktops.
 	// Might save us if other plugins interfere with workspace creation/deletion
 	let loop_counter = 0;
-	const loop_limit = workspace.desktops.length;
-	for (; desktop_idx < workspace.desktops.length && loop_counter < loop_limit; ++desktop_idx)
+	const desktopsLength = compat.workspaceDesktops().length;
+	for (; desktop_idx < desktopsLength && loop_counter < desktopsLength; ++desktop_idx)
 	{
 		loop_counter += 1;
 		if (is_empty_desktop(desktop_idx))
@@ -178,9 +256,9 @@ function on_desktop_switch(old_desktop)
 
 // actions relating to creating desktops
 // also this subscribes all clients to their desktopsChanged event
-workspace.windowAdded.connect(on_client_added);
+compat.windowAddedSignal(workspace).connect(on_client_added);
 // also do this for all existing clients
-workspace.windowList().forEach(on_client_added);
+compat.windowList(workspace).forEach(on_client_added);
 
 // handle change desktop events
 workspace.currentDesktopChanged.connect((old_desktop) => { on_desktop_switch(old_desktop); });
